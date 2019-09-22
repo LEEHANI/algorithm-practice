a8256173f3bfa407178d38dc68b653334951802d
LEEHANI <rorean@naver.com>
2019-09-22 16:03:16 +0900

    15664 N과M(10)

---
+++ NAndM/_15664_N과M_10.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class _15664_N과M_10
+{
+	static int N, M;
+	static ArrayList<Integer> arr = new ArrayList<Integer>();
+	static int result[];
+	
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args)
+	{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		try
+		{
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			result = new int[M];
+			
+			st = new StringTokenizer(bf.readLine());
+			
+			for(int i = 0 ; i < N; i ++)
+			{
+				arr.add(Integer.parseInt(st.nextToken()));
+			}
+			
+			Collections.sort(arr);
+			
+			permutation(result, N, M, 0, 0);
+			
+			bw.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void permutation(int[] result, int N, int M, int cnt, int start)
+	{
+		if(M == cnt)
+		{
+			try
+			{
+				for(int i = 0 ; i < M; i ++)
+				{
+					bw.write(result[i] + " ");
+				}
+				bw.write("\n");
+				bw.flush();
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+			return;
+		}
+		else
+		{
+			int last = 0;
+			
+			for(int i = start ; i < N; i ++)
+			{
+				int temp = arr.get(i);
+				
+				if(last != temp)
+				{
+					result[cnt] = temp;
+					
+					permutation(result, N, M, cnt+1, i+1);
+					
+					last = result[cnt];
+				}
+			}
+		}
+	}
+}
