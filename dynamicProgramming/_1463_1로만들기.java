ed09246df797df3fd264d671e313600c4d838702
LEEHANI <rorean@naver.com>
2019-09-23 19:55:14 +0900

    1463 1로만들기

--- dynamicProgramming/_1463_1로만들기.java
+++ dynamicProgramming/_1463_1로만들기.java
@@ -10,11 +10,11 @@ public class _1463_1로만들기
 	static int n;
 	static int dp[]; // 정수 n이 주어졌을 때, n을 1로 만드는 최소횟수
 	
-	// 2019.09.14
 	// -1, %3, %2 다 해봐서 최솟값을 dp에 저장해놓기!
-	// dp[s] = dp[n-1] + 1
-	// dp[s] = dp[n/3] + 1
-	// dp[s] = dp[n/2] + 1
+	// -1, %3, %2 하면 연산 한번 했으므로 +1 꼭 해줘야함!!
+	// dp[n] = dp[n-1] + 1  <-- n-1(전 단계)의 최솟값 + 1( -1로 한번 연산을 진행했으므로 1 더해줘야한다)   
+	// if(n %2 == 0) dp[n] = dp[n/2] + 1
+	// if(n %3 == 0) dp[n] = dp[n/3] + 1
 	
 	public static void main(String[] args)
 	{
@@ -110,6 +110,37 @@ public class _1463_1로만들기
 		return dp[s];
 	}
 	
+	public static int bottomUpVersion2(int n)
+	{
+		try
+		{
+			dp[1] = 0;
+			dp[2] = 1;
+			dp[3] = 1;
+			
+			for(int i = 4; i <= n ; i ++)
+			{
+				dp[i] = dp[i-1] +1;
+				
+				if(i % 2 == 0)
+				{
+					dp[i] = Math.min(dp[i], dp[i/2]+1);
+				}
+				
+				if(i % 3 == 0)
+				{
+					dp[i] = Math.min(dp[i], dp[i/3]+1);
+				}
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			// n이 3이하면 에러 발생하므로 방지용임
+		}
+		
+		return dp[n];
+	}
+	
 	// 시간초과
 	public static int topDown(int s)
 	{
@@ -130,8 +161,6 @@ public class _1463_1로만들기
 		
 		if(s % 3 == 0)
 		{
-			// 기존 dp[s]과 3으로 나눴을때의 둘 중 최솟값
-			// 연산이 1번 들어가는거니까 1 더해줘야함 
 			return dp[s] = Math.min(dp[s], topDown(s/3) + 1);
 		}
 		
